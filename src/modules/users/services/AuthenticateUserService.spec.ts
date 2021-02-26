e7ea71006565f67dc7a7b62dfcde2ebc13fa4349
fbsoares-lu <lsoareshfb@gmail.com>
2021-02-26 16:28:33 -0300

    Create a TDD of UserAutenticated

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -15,7 +15,7 @@ describe('AuthenticateUser', () => {
             fakeUserRepository,
             fakeHashProvider
         );
-        
+
         const createUserService = new CreateUserService(
             fakeUserRepository,
             fakeHashProvider
@@ -33,5 +33,50 @@ describe('AuthenticateUser', () => {
         });
 
         expect(authenticate).toHaveProperty('token');
+    });
+
+    it('should not be able to authenticate with non existing user', async () => {
+        const fakeUserRepository = new FakeUserRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const authenticateUser = new AuthenticateUserService(
+            fakeUserRepository,
+            fakeHashProvider
+        );
+
+        expect(
+            authenticateUser.execute({
+                email: 'johndoe@email.com',
+                password: '123123'
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to authenticate with wrong password', async () => {
+        const fakeUserRepository = new FakeUserRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const authenticateUser = new AuthenticateUserService(
+            fakeUserRepository,
+            fakeHashProvider
+        );
+
+        const createUserService = new CreateUserService(
+            fakeUserRepository,
+            fakeHashProvider
+        );
+
+        await createUserService.execute({
+            name: 'John Doe',
+            email: 'johndoe@email.com',
+            password: '123123'
+        });
+
+        expect(
+            authenticateUser.execute({
+                email: 'johndoe@email.com',
+                password: 'wrong-password'
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
